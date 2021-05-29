d31965e40a45c2f9cdae43c8e71dad3a85ab562d
Nimisharajesh <74758694+Nimisharajesh@users.noreply.github.com>
2021-05-29 11:37:40 +0530

    Add files via upload

---
+++ list.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Returns the list of cars.
+ */
+require 'connect.php';
+    
+$cars = [];
+$sql = "SELECT  * FROM stock_data";
+
+if($result = mysqli_query($con,$sql))
+{
+  $cr = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $cars[$cr]['id']    = $row['id'];
+    $cars[$cr]['name'] = $row['Name'];
+    $cr++;
+  }
+    
+  echo json_encode(['data'=>$cars]);
+}
+else
+{
+  http_response_code(404);
+}
